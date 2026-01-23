ae2995d43e0343c27052aa3fed41b028bf6455a6
Willayam <williamlarsten@gmail.com>
2026-01-23 19:13:14 +0100

    feat(quiz): add quiz components index

---
+++ apps/mobile/components/quiz/index.ts
@@ -0,0 +1,6 @@
+// apps/mobile/components/quiz/index.ts
+
+export { OptionButton, type OptionState } from './OptionButton';
+export { QuizHeader } from './QuizHeader';
+export { ExplanationCard } from './ExplanationCard';
+export { ExitModal } from './ExitModal';
